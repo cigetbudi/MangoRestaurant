2ffb7d0e5ed5cc20337adef25980d8e6bc3f4ef9
Sigit Budiarmawan <cigetbudi@outlook.com>
2022-03-18 19:57:50 +0700

    Configuring ProductService and HttpClient in Program.cs (FrontEnd)

--- Mango.Web/Program.cs
+++ Mango.Web/Program.cs
@@ -1,7 +1,18 @@
+using Mango.Web;
+using Mango.Web.Services;
+using Mango.Web.Services.IServices;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+//HttpSetting DI
+builder.Services.AddHttpClient<IProductService, ProductService>();
+SD.ProductAPIBase = builder.Configuration["ServiceUrls:ProductAPI"];
+
+//Inject Service DI
+builder.Services.AddScoped<IProductService, ProductService>();
 
 var app = builder.Build();
 
