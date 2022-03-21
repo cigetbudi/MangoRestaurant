4307ddf42c4208cd95c4403caa3d59bf69c7fc5c
Sigit Budiarmawan <cigetbudi@outlook.com>
2022-03-21 18:28:01 +0700

    Configure and run Identity Server

--- Mango.Services.Identity/SD.cs
+++ Mango.Services.Identity/SD.cs
@@ -47,7 +47,6 @@ namespace Mango.Services.Identity
                         IdentityServerConstants.StandardScopes.Profile,
                         IdentityServerConstants.StandardScopes.Email,
                         "mango"
-
                     }
                 },
             };
